595ffa36485b69769db9f2d41f7d2bdaf4545bd5
ricar <ricardocisneros197@gmail.com>
2023-03-01 02:29:53 -0800

    learned how to use the asterisk for the html

--- Develop/server.js
+++ Develop/server.js
@@ -4,20 +4,22 @@ const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 const path = require("path");
 
-
+app.use(express.json());
 
 // Middleware for parsing JSON
 const PORT = process.env.port || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.json());
+
+
+
+
 
 
 // Route for notes.html
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
-
 
 
 app.get("/api/notes", (req, res) => {
@@ -29,12 +31,14 @@ app.get("/api/notes", (req, res) => {
 
 app.post("/api/notes", (req, res) => {
   // Create a new note object with the given ID and the properties from the request body
-  const title = req.body.title 
-    const text = req.body.text 
-    const newNote = {title, text, id:uuidv4()}
+  const newNote = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
 
   // Read the existing notes from the db.json file
-  fs.readFile("./db/db.json",  (err, data) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
     // Parse the existing notes into an array
     const notes = JSON.parse(data);
 
@@ -48,13 +52,13 @@ app.post("/api/notes", (req, res) => {
       // console.log('newNote:', newNote);
       // console.log('notes:', notes);
     });
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
   });
 });
 // Route for index.html (catch-all route)
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"))
 })
+
 // Start the server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
